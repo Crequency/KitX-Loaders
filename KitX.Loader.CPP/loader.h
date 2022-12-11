549650342b33baf3d16b96fab59af66bc6b6ad86
xxxxxxxb <914401776@qq.com>
2022-12-11 22:53:42 +0800

    Feat(Kitx.Loader.CPP)新增cpploader项目

---
+++ KitX.Loader.CPP/loader.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include <winsock2.h>
+#include "configor/json.hpp"
+using namespace configor;
+namespace KitX {
+    class Plugin {
+    private:
+        json::value pluginStruct;
+    public:
+        Plugin(std::string myPluginName,std::string myAuthName){
+            pluginStruct["pluginName"] = myPluginName;
+            pluginStruct["authName"] = myAuthName;
+        }
+        int beginConnect(std::string address, int port, std::string myPluginStruct) {
+            //cpp socket 通信,参数有地址,端口,插件信息(pluginStruct序列化之后的字符串),地址和端口插件启动时会以命令行参数的形式传入(在文档里约定)
+            return 0;
+        }
+    };
+}
+
---
+++ KitX.Loader.CPP/plugin.cpp
@@ -0,0 +1,7 @@
+﻿#include "loader.h"
+
+
+int main(int argc, std::string argv[] ) {
+    KitX::Plugin myplugin("myplugin" , "myname");
+}
+
