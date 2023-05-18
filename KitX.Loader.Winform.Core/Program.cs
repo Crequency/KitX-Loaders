146dcac51f06165c99108fbbfdd2f313e83b05e5
Dynesshely <catrol@qq.com>
2023-05-18 17:33:25 +0800

    🧩 Refactor(Loader.WPF, Loader.Winform): Use `KitX.Loader.CSharp` as base loader.

--- KitX.Loader.Winform.Core/Program.cs
+++ KitX.Loader.Winform.Core/Program.cs
@@ -1,17 +1,21 @@
-namespace KitX.Loader.Winform.Core
+﻿using KitX.Loader.CSharp;
+
+namespace KitX.Loader.Winform.Core;
+
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            //Application.Run(new Form1());
-        }
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+
+        //Application.Run(new Form1());
+
+        ArgsParser.Parse(Environment.GetCommandLineArgs());
     }
 }
