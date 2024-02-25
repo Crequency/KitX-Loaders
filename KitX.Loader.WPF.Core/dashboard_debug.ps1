ff71cbe717f03ad6cb66849086eae753093d19f4
Dynesshely <catrol@qq.com>
2024-02-25 19:10:39 +0800

    💾 Feat: The first time that dashboard invoke plugin function throw network connection.

---
+++ KitX.Loader.WPF.Core/dashboard_debug.ps1
@@ -0,0 +1,18 @@
+﻿param(
+    [int]
+    $From = 1,
+
+    [int]
+    $To = $(throw "`To` not provide"),
+
+    [int]
+    $Port = $(throw "`Port` not provide"),
+
+    [double]
+    $Sleep = 0.7
+)
+
+$From..$To | ForEach-Object {
+    .\bin\Debug\net8.0-windows\KitX.Loader.WPF.Core.exe --load '..\..\KitX Plugins\TestPlugin.WPF.Core\bin\Debug\net8.0-windows\TestPlugin.WPF.Core.dll' --connect "ws://127.0.0.1:$Port/connectionId_$_";
+    Start-Sleep $Sleep;
+}
